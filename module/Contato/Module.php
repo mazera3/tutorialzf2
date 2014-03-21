3884d538d6aa9ed4f8f7b0f0e134cc0eb62e4fe4
mazera <mazera@mazera-G31M-ES2C>
2014-03-20 23:33:17 -0300

    Agenda de contatos - tutorial - parte 7

--- module/Contato/Module.php
+++ module/Contato/Module.php
@@ -4,6 +4,14 @@
 */
 namespace Contato;
 
+// import Model\Contato
+use Contato\Model\Contato,
+    Contato\Model\ContatoTable;
+
+// import Zend\Db
+use Zend\Db\ResultSet\ResultSet,
+    Zend\Db\TableGateway\TableGateway;
+
 class Module
 {
     /**
@@ -13,7 +21,7 @@ class Module
     {
         return include __DIR__ . '/config/module.config.php';
     }
-/**
+    /**
      * autoloader para nosso modulo
      */
     public function getAutoloaderConfig()
@@ -27,20 +35,46 @@ class Module
         );
     }
     /**
- * Register View Helper
- */
-public function getViewHelperConfig()
-{
-    return array(
-        # registrar View Helper com injecao de dependecia
-        'factories' => array(
-            'menuAtivo'  => function($sm) {
-                return new View\Helper\MenuAtivo($sm->getServiceLocator()->get('Request'));
-            },
-            'message' => function($sm) {
-                return new View\Helper\Message($sm->getServiceLocator()->get('ControllerPluginManager')->get('flashmessenger'));
-            },
-        )
-    );
-}
+     * Register View Helper
+     */
+    public function getViewHelperConfig()
+    {
+        return array(
+            # registrar View Helper com injecao de dependecia
+            'factories' => array(
+                'menuAtivo'  => function($sm) {
+                    return new View\Helper\MenuAtivo($sm->getServiceLocator()->get('Request'));
+                },
+                'message' => function($sm) {
+                    return new View\Helper\Message($sm->getServiceLocator()->get('ControllerPluginManager')->get('flashmessenger'));
+                },
+            )
+        );
+    }
+    /**
+     * Register Services
+     */
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'ContatoTableGateway' => function ($sm) {
+                    // obter adapter db atraves do service manager
+                   // $adapter = $sm->get('AdapterDb');
+                    $adapter = $sm->get('Zend\Db\Adapter\Adapter');
+
+                    // configurar ResultSet com nosso model Contato
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Contato());
+
+                    // return TableGateway configurado para nosso model Contato
+                    return new TableGateway('contatos', $adapter, null, $resultSetPrototype);
+                },
+                'ModelContato' => function ($sm) {
+                    // return instacia Model ContatoTable
+                    return new ContatoTable($sm->get('ContatoTableGateway'));
+                }
+            )
+        );
+    }
 }
--- module/Contato/src/Contato/Controller/ContatosController.php
+++ module/Contato/src/Contato/Controller/ContatosController.php
@@ -1,14 +1,25 @@
 <?php
 
+/**
+ * namespace de localizacao do nosso controller
+ */
 namespace Contato\Controller;
 
+// import Zend\Mvc
 use Zend\Mvc\Controller\AbstractActionController;
+// import Zend\View
+use Zend\View\Model\ViewModel;
 
 class ContatosController extends AbstractActionController
 {
+
+    protected $contatoTable;
+
     // GET /contatos
     public function indexAction()
     {
+        // enviar para view o array com key contatos e value com todos os contatos
+        return new ViewModel(array('contatos' => $this->getContatoTable()->fetchAll()));
     }
 
     // GET /contatos/novo
@@ -17,37 +28,36 @@ class ContatosController extends AbstractActionController
     }
 
     // POST /contatos/adicionar
-public function adicionarAction()
-{
-    // obtém a requisição
-    $request = $this->getRequest();
+    public function adicionarAction()
+    {
+        // obtém a requisição
+        $request = $this->getRequest();
 
-    // verifica se a requisição é do tipo post
-    if ($request->isPost()) {
-        // obter e armazenar valores do post
-        $postData = $request->getPost()->toArray();
-        $formularioValido = true;
-        // $formularioValido = false;
+        // verifica se a requisição é do tipo post
+        if ($request->isPost()) {
+            // obter e armazenar valores do post
+            $postData = $request->getPost()->toArray();
+            $formularioValido = true;
 
-        // verifica se o formulário segue a validação proposta
-        if ($formularioValido) {
-            // aqui vai a lógica para adicionar os dados à tabela no banco
-            // 1 - solicitar serviço para pegar o model responsável pela adição
-            // 2 - inserir dados no banco pelo model
-            // adicionar mensagem de sucesso
-            $this->flashMessenger()->addSuccessMessage("Contato criado com sucesso");
+            // verifica se o formulário segue a validação proposta
+            if ($formularioValido) {
+                // aqui vai a lógica para adicionar os dados à tabela no banco
+                // 1 - solicitar serviço para pegar o model responsável pela adição
+                // 2 - inserir dados no banco pelo model
+                // adicionar mensagem de sucesso
+                $this->flashMessenger()->addSuccessMessage("Contato criado com sucesso");
 
-            // redirecionar para action index no controller contatos
-            return $this->redirect()->toRoute('contatos');
-        } else {
-            // adicionar mensagem de erro
-            $this->flashMessenger()->addErrorMessage("Erro ao criar contato");
+                // redirecionar para action index no controller contatos
+                return $this->redirect()->toRoute('contatos');
+            } else {
+                // adicionar mensagem de erro
+                $this->flashMessenger()->addErrorMessage("Erro ao criar contato");
 
-            // redirecionar para action novo no controllers contatos
-            return $this->redirect()->toRoute('contatos', array('action' => 'novo'));
+                // redirecionar para action novo no controllers contatos
+                return $this->redirect()->toRoute('contatos', array('action' => 'novo'));
+            }
         }
     }
-}
 
     // GET /contatos/detalhes/id
     public function detalhesAction()
@@ -64,17 +74,19 @@ public function adicionarAction()
             return $this->redirect()->toRoute('contatos');
         }
 
-        // aqui vai a lógica para pegar os dados referente ao contato
-        // 1 - solicitar serviço para pegar o model responsável pelo find
-        // 2 - solicitar form com dados desse contato encontrado
-        // formulário com dados preenchidos
-        $form = array(
-            'nome' => 'Igor Rocha',
-            "telefone_principal" => "(085) 8585-8585",
-            "telefone_secundario" => "(085) 8585-8585",
-            "data_criacao" => "02/03/2013",
-            "data_atualizacao" => "02/03/2013",
-        );
+        try {
+            // aqui vai a lógica para pegar os dados referente ao contato
+            // 1 - solicitar serviço para pegar o model responsável pelo find
+            // 2 - solicitar form com dados desse contato encontrado
+            // formulário com dados preenchidos
+            $form = (array) $this->getContatoTable()->find($id);
+        } catch (\Exception $exc) {
+            // adicionar mensagem
+            $this->flashMessenger()->addErrorMessage($exc->getMessage());
+
+            // redirecionar para action index
+            return $this->redirect()->toRoute('contatos');
+        }
 
         // dados eviados para detalhes.phtml
         return array('id' => $id, 'form' => $form);
@@ -95,22 +107,25 @@ public function adicionarAction()
             return $this->redirect()->toRoute('contatos');
         }
 
-        // aqui vai a lógica para pegar os dados referente ao contato
-        // 1 - solicitar serviço para pegar o model responsável pelo find
-        // 2 - solicitar form com dados desse contato encontrado
+        try {
+            // aqui vai a lógica para pegar os dados referente ao contato
+            // 1 - solicitar serviço para pegar o model responsável pelo find
+            // 2 - solicitar form com dados desse contato encontrado
+            // formulário com dados preenchidos
+            $form = (array) $this->getContatoTable()->find($id);
+        } catch (\Exception $exc) {
+            // adicionar mensagem
+            $this->flashMessenger()->addErrorMessage($exc->getMessage());
 
-        // formulário com dados preenchidos
-        $form = array(
-            'nome'                  => 'Igor Rocha',
-            "telefone_principal"    => "(085) 8585-8585",
-            "telefone_secundario"   => "(085) 8585-8585",
-        );
+            // redirecionar para action index
+            return $this->redirect()->toRoute('contatos');
+        }
 
         // dados eviados para editar.phtml
         return array('id' => $id, 'form' => $form);
     }
 
-     // PUT /contatos/editar/id
+    // PUT /contatos/editar/id
     public function atualizarAction()
     {
         // obtém a requisição
@@ -127,7 +142,6 @@ public function adicionarAction()
                 // aqui vai a lógica para editar os dados à tabela no banco
                 // 1 - solicitar serviço para pegar o model responsável pela atualização
                 // 2 - editar dados no banco pelo model
-
                 // adicionar mensagem de sucesso
                 $this->flashMessenger()->addSuccessMessage("Contato editado com sucesso");
 
@@ -153,18 +167,30 @@ public function adicionarAction()
         if (!$id) {
             // adicionar mensagem de erro
             $this->flashMessenger()->addMessage("Contato não encotrado");
-
         } else {
             // aqui vai a lógica para deletar o contato no banco
             // 1 - solicitar serviço para pegar o model responsável pelo delete
             // 2 - deleta contato
-
             // adicionar mensagem de sucesso
             $this->flashMessenger()->addSuccessMessage("Contato de ID $id deletado com sucesso");
-
         }
 
         // redirecionar para action index
         return $this->redirect()->toRoute('contatos');
+    }
+
+    /**
+     * Metodo privado para obter instacia do Model ContatoTable
+     * 
+     * @return \Contato\Model\ContatoTable
+     */
+    private function getContatoTable()
+    {
+        // adicionar service ModelContato a variavel de classe
+        if (!$this->contatoTable)
+            $this->contatoTable = $this->getServiceLocator()->get('ModelContato');
+
+        // return vairavel de classe com service ModelContato
+        return $this->contatoTable;
     }
 }
